def7b4ed24a9667620e5b1d976c68cf497776dff
Ronny Chamba <ronnychamba96@gmail.com>
2025-04-04 10:54:35 -0500

    refactor: add version app

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   urlMsVeagro:"https://dev.olimpush.com/apiveagro",
+  version: '1.2.0',
   firebase: {
     apiKey: "AIzaSyAIeyD37PibTTWW0_b2FVnPE8MvLnPV16U",
     authDomain: "la-gotita-2024.firebaseapp.com",
