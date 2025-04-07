d948cd8bb33a70ee83cbbf0bb6802f9bd33a481e
daniel vera <desarrollandoideas8@gmail.com>
2025-04-07 13:56:16 -0500

    feat: actualizar dependencias de chart.js y primeng, y agregar gráficos al componente de dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -98,7 +98,7 @@ export const routes: Routes & {
 
       {
         path: '**',
-        redirectTo: 'pedidos',
+        redirectTo: 'inicio',
         pathMatch: 'full',
       },
     ],
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, inject, PLATFORM_ID, signal } from '@angular/core';
 import { PedidosService } from '../../services';
+import { isPlatformBrowser } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './dashboard.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -13,6 +15,7 @@ export class DashboardComponent {
 
   constructor(
     private readonly pedidoService: PedidosService,
+    private cd: ChangeDetectorRef,
 
   ) {
     this.getProduct();
@@ -42,4 +45,120 @@ export class DashboardComponent {
       this.dataDoc.set(data);
     });
   }
+
+
+  data: any;
+
+  options: any;
+
+  platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    this.initChart();
+    this.initChartRounde();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Dataset 1',
+            borderColor: documentStyle.getPropertyValue('--p-orange-500'),
+            borderWidth: 2,
+            fill: false,
+            tension: 0.4,
+            data: [50, 25, 12, 48, 56, 76, 42],
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: documentStyle.getPropertyValue('--p-gray-500'),
+            data: [21, 84, 24, 75, 37, 65, 34],
+            borderColor: 'white',
+            borderWidth: 2,
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
+            data: [41, 52, 24, 74, 23, 21, 32],
+          },
+        ],
+      };
+
+      this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 1,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+      this.cd.markForCheck();
+    }
+  }
+
+  dataRounde: any;
+
+  optionsRpunde: any;
+
+  initChartRounde() {
+    if (isPlatformBrowser(this.platformId)) {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+
+        this.dataRounde = {
+            labels: ['A', 'B', 'C'],
+            datasets: [
+                {
+                    data: [540, 325, 702],
+                    backgroundColor: [documentStyle.getPropertyValue('--p-cyan-500'), documentStyle.getPropertyValue('--p-orange-500'), documentStyle.getPropertyValue('--p-gray-500')],
+                    hoverBackgroundColor: [documentStyle.getPropertyValue('--p-cyan-400'), documentStyle.getPropertyValue('--p-orange-400'), documentStyle.getPropertyValue('--p-gray-400')]
+                }
+            ]
+        };
+
+        this.optionsRpunde = {
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true,
+                        color: textColor
+                    }
+                }
+            }
+        };
+        this.cd.markForCheck()
+    }
+
+}
 }
